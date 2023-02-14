82003a8c6b7b6428fe4d5a0ff418214599d5ea38
jannis2301 <jannis.ax@gmx.de>
2023-02-14 14:18:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# **CocktailNeeds**
+
+CocktailNeeds is an app that allows users to search for they preferred cocktail.
+
+## Table of Contents
+
+- [Project Prompt](#project-prompt)
+- [Technologies](#technologies)
+- [View the App](#view-the-app)
+
+## Project Prompt
+
+In this project, we have build an application that allows users to search for their favorite cocktail and get some infos, like the glass it is served, ingredients or the instructions.
+
+## Technologies
+
+- React JS
+
+## View the App
+
+-On [Netlify](htpps:)
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, vel
+        tempora eaque repudiandae minus rerum sapiente consectetur error id quae
+        ullam, dignissimos, expedita alias? Libero, cum amet quod exercitationem
+        nostrum quis ex eos ducimus facilis placeat autem in dolore ut?
+      </p>
+    </section>
+  );
+};
+
+export default About;
